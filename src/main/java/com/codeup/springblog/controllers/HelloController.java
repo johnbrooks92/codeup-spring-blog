4d2c85adce5b3612af88b17992564818bd04bb5f
John <johntbrooks92@gmail.com>
2021-02-11 09:22:31 -0600

    MathController Created

--- src/main/java/com/codeup/springblog/controllers/HelloController.java
+++ src/main/java/com/codeup/springblog/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package controllers;
+package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
--- src/test/java/com/codeup/springblog/CodeupSpringBlogApplicationTests.java
+++ src/test/java/com/codeup/springblog/CodeupSpringBlogApplicationTests.java
@@ -1,4 +1,4 @@
-package com.codeup.codeupspringblog;
+package com.codeup.springblog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

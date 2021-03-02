ada0f3e5ba11bb3d4040e69c5c212a2cba26bdca
John <johntbrooks92@gmail.com>
2021-03-02 11:43:32 -0600

    landing page fix attempt

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -10,6 +10,6 @@ public class HomeController {
     @GetMapping("/")
     @ResponseBody
     public String home() {
-        return "This is the landing page!";
+        return "redirect:/posts";
     }
 }
